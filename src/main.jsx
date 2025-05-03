b7fac21f826248f6d3d90d45a23cdf10c0e78356
vrdesai0 <v.r.desai0@gmail.com>
2025-05-03 19:03:43 +0530

    df

--- src/main.jsx
+++ src/main.jsx
@@ -4,7 +4,7 @@ import { BrowserRouter } from 'react-router-dom'
 
 
 createRoot(document.getElementById('root')).render(
-  <BrowserRouter>
+  <BrowserRouter  basename="/Metro_Masala">
     <App />
   </BrowserRouter>
 )
