cfe2bb25b07c5a0b24336881867499db3836f324
vrdesai0 <v.r.desai0@gmail.com>
2025-05-04 00:43:43 +0530

    res

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,7 @@
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 import { BrowserRouter } from 'react-router-dom'
+import './script.js';
 
 
 createRoot(document.getElementById('root')).render(
